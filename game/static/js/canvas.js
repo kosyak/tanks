4c6d89c660b267ca91584eac220078928346f932
Vadim Kosov <vc.kosyak@gmail.com>
2011-11-13 01:39:41 +0400

    'clientWidth' instead of 'innerWidth'
    Signed-off-by: Vadim Kosov <vc.kosyak@gmail.com>

--- game/static/js/canvas.js
+++ game/static/js/canvas.js
@@ -19,8 +19,8 @@ var CanvasBlackjack = (function(id) {
     }
     function renderField() {
         var x, y;
-        for (x = 0; x < canvas.innerWidth; x += block_size) {
-            for (y = 0; y < canvas.innerHeight; y += block_size) {
+        for (x = 0; x < canvas.clientWidth; x += block_size) {
+            for (y = 0; y < canvas.clientHeight; y += block_size) {
                 context.fillStyle = ((x + y) % 2) ? '#000000' : '#ffffff';
                 context.fillRect(x, y, block_size, block_size);
             }
