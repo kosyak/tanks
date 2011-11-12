36b98f7e62d2589b3b70a8fba81442a6cb2c94f5
Vadim Kosov <vc.kosyak@gmail.com>
2011-11-12 16:13:12 +0400

    added SIGTERM handling for dot cloud
    Signed-off-by: Vadim Kosov <vc.kosyak@gmail.com>

--- game/server.js
+++ game/server.js
@@ -1,3 +1,5 @@
+// http://docs.dotcloud.com/services/nodejs/
+
 require('http').createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   output = "Fucking Tanks Are Where?!\n";
@@ -6,3 +8,12 @@ require('http').createServer(function (request, response) {
   }
   response.end(output);
 }).listen(8080);
+
+/*
+process.on('SIGTERM', function () {
+    console.log('Got SIGTERM exiting...');
+    // do some cleanup here
+    process.exit(0);
+});
+
+*/
