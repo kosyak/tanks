52646b29a3182871799aa38bf6e58de3eaea07be
Vadim Kosov <vc.kosyak@gmail.com>
2011-11-13 17:38:06 +0400

    Added vLog to html
    Signed-off-by: Vadim Kosov <vc.kosyak@gmail.com>

--- game/static/js/vlog.js
+++ game/static/js/vlog.js
@@ -3,6 +3,10 @@
 var vLog = (function(id) {
     var container = document.getElementById(id);
     function log(msg) {
+        if (!container || !container.innerHTML) {
+            console.error('No vLog to write found');
+            return false;
+        }
         var date = new Date();
         if (msg.toString()) {
             container.innerHTML = '[' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '] ' + msg.toString();
