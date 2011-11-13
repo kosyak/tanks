418239128e46a1a0b29381557e55e3d8dfee7731
Vadim Kosov <vc.kosyak@gmail.com>
2011-11-13 21:07:58 +0400

    Added .gitignore; dummy move implementation
    Signed-off-by: Vadim Kosov <vc.kosyak@gmail.com>

--- game/static/js/tank.js
+++ game/static/js/tank.js
@@ -6,16 +6,36 @@
  **/
 
 function Tank(context, callback) {
-    this.img_src = '/img/tank_top.png';
+    this.imgURI = '/img/tank_top.png'; // URI! Yo!
     this.img = new Image();
-    this.img.src = this.img_src;
+    this.img.src = this.imgURI;
     if (typeof callback === 'function') {
-        this.img.onload = callback;
+        this.img.onload = function() { this.lastMove = new Date(); callback(); } 
     }
+    this.deltaChar = {}
 
-    this.context = context;    
+    this.context = context;
+    
+    this.speed = 0.1; // pixels per millisecond
+    this.lastMove = new Date();
 }
 
 Tank.prototype.place = function(x, y) {
     this.context.drawImage(this.img, x, y);
+}
+
+Tank.prototype.deltaFromCharCode = function(char_code) {
+    var keys_deltas = {
+        'w': { x: 0,  y: -1 },
+        'a': { x: -1, y: 0 },
+        's': { x: 0,  y: 1 },
+        'd': { x: 1,  y: 0 }
+    };
+    return keys_deltas[String.fromCharCode(char_code)];
+}
+
+Tank.prototype.move = function(char_code) {
+    var delta = this.deltaFromCharCode(char_code),
+        time = new Date(); // TODO: check Date.now() compatibility and use if possible
+    // if (this)
 }
